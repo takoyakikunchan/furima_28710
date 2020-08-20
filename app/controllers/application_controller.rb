4c042541191867281ad087cd3d24a356d515d146
takoyakikunchan <66656447+takoyakikunchan@users.noreply.github.com>
2020-08-20 16:50:20 +0900

    deviseの追加カラムの保存許可

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :given_name, :read_given_name, :read_fam_name, :birthday_id ])
+  end
 end
