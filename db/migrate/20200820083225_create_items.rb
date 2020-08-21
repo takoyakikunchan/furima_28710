e5baa6a541db13709e6baf8d5042d780f7069ce1
takoyakikunchan <66656447+takoyakikunchan@users.noreply.github.com>
2020-08-21 17:13:42 +0900

    商品テーブルの作成

---
+++ db/migrate/20200820083225_create_items.rb
@@ -0,0 +1,16 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :image,null: false
+      t.text :description,null: false
+      t.integer :price,null: false
+      t.references :user,null: false, foreign_key: true
+      t.integer :category_id  ,null: false
+      t.integer :condition_id ,null: false
+      t.integer :shipping_fee_person_id,null: false
+      t.integer :date_ish_id,null: false
+      t.integer :region_id,null: false
+      t.timestamps
+    end
+  end
+end
