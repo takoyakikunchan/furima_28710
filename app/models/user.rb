9b97a0ba6797a79b7f0a97aec5cc39debd444d86
takoyakikunchan <66656447+takoyakikunchan@users.noreply.github.com>
2020-08-21 13:32:09 +0900

    ヴァリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          validates :nickname, presence: true
-         validates :email, { presence: true, uniqueness: true, inclusion: { in: ["@"] }}  
-        validates :password,{ presence: true, confirmation: true, length: {minimum: 6}} 
-        PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-        validates_format_of :password, with: PASSWORD_REGEX
-        
-        with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
-          validates :given_name
-          validates :family_name
-        end
-      with_options presence: true, format: { with: /\A[\p{katakana}\u{30fc}]+\z/ } do
-       validates :read_fam_name
-       validates :read_given_name
-     end
+         
 
-    validates :birthday, presence: true
+  validates :password,{ presence: true, confirmation: true, length: {minimum: 6}} 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+        
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+    validates :given_name
+    validates :family_name
+  end
+  with_options presence: true, format: { with: /\A[\p{katakana}\u{30fc}]+\z/ } do
+    validates :read_fam_name
+    validates :read_given_name
+  end
+
+  validates :birthday, presence: true
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,5 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"abe"}
-    email                 {"kkk@gmail.com"}
-    password              {"knbn0000"}
-    password_confirmation {password}
-    given_name            {"相原"}
-    family_name           {"太郎"}
-    read_fam_name        {"アイハラ"}
-    read_given_name       {"タロウ"}
-    birthday                {21.years.ago}
-  end　
+    
+  end
 end
