638776e1421ea08a03d9767a83edd9994580413f
takoyakikunchan <66656447+takoyakikunchan@users.noreply.github.com>
2020-08-23 08:12:47 +0900

    エラー修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   belongs_to_active_hash :category
 
   #空の投稿を保存できないようにする
-  validates :name, :image,:price,:description :category, presence: true
+  validates :name, :image,:price,:description,:category, presence: true
 
   #ジャンルの選択が「--」の時は保存できないようにする
   validates :category_id, numericality: { other_than: 1 } 
