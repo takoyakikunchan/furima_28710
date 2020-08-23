368be393d604300b80774c118ced45e7224a0830
takoyakikunchan <66656447+takoyakikunchan@users.noreply.github.com>
2020-08-23 16:43:58 +0900

    未ログインユーザーの遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_login, only: [:new]
+
   def index
   end
 
@@ -12,6 +14,12 @@ class ItemsController < ApplicationController
         redirect_to root_path
       else
         render :new
+    end
+  end
+
+  def move_to_login
+    unless user_signed_in?
+      redirect_to "/users/sign_in"
     end
   end
 
