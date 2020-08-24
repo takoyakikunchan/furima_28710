2365309bb9b814107ca4f24db6d219c64af3dccf
takoyakikunchan <66656447+takoyakikunchan@users.noreply.github.com>
2020-08-24 13:00:52 +0900

    パスをprefixに修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,7 +18,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_login
-    redirect_to '/users/sign_in' unless user_signed_in?
+    redirect_to new_user_session_path unless user_signed_in?
   end
 
   private
