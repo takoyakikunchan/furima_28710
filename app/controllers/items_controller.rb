24d55272d1ca370acdfd446a08c6e283beceb683
takoyakikunchan <66656447+takoyakikunchan@users.noreply.github.com>
2020-08-24 22:55:52 +0900

    コントロール

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   def move_to_login
     redirect_to new_user_session_path unless user_signed_in?
   end
