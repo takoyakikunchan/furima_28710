aea17b6b65420a4272bf733e479dbb72d9b2977f
takoyakikunchan <66656447+takoyakikunchan@users.noreply.github.com>
2020-08-25 10:39:24 +0900

    beforeactionでまとめる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_login, only: [:new]
-
+  before_action :set_item, only: [:show]
   def index
     @items = Item.all.includes(:user).order(id: 'DESC')
   end
@@ -19,7 +19,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def move_to_login
@@ -30,5 +29,9 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :image, :description, :price, :category_id, :condition_id, :shipping_fee_person_id, :region_id, :date_ish_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
