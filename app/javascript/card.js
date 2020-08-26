2b05652266b9463249e69a9f9b07e8f0bef6ef4d
takoyakikunchan <66656447+takoyakikunchan@users.noreply.github.com>
2020-08-26 18:28:39 +0900

    card.jsの実装

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,6 +1,7 @@
 const pay = () => {
   Payjp.setPublicKey("pk_test_1a9773a65bad3e68e6673d65");// PAY.JPテスト公開鍵
   const form = document.getElementById("charge-form");
+  console.log(form)
   form.addEventListener("submit", (e) => {
     e.preventDefault();
 
@@ -13,18 +14,21 @@ const pay = () => {
       exp_month: formData.get("exp_month"),
       exp_year: `20${formData.get("exp_year")}`,
     };
+    console.log(card)
 
     Payjp.createToken(card, (status, response) => {
+      console.log(status)
+      console.table(response)
       if (status === 200) {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
         const tokenObj = `<input value=${token} type="hidden" name='token'>`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
 
-        document.getElementById("number").removeAttribute("name");
-        document.getElementById("cvc").removeAttribute("name");
-        document.getElementById("exp_month").removeAttribute("name");
-        document.getElementById("exp_year").removeAttribute("name");
+        document.getElementById("card-number").removeAttribute("name");
+        document.getElementById("card-cvc").removeAttribute("name");
+        document.getElementById("card-exp-month").removeAttribute("name");
+        document.getElementById("card-exp-year").removeAttribute("name");
 
         document.getElementById("charge-form").submit();
         document.getElementById("charge-form").reset();
