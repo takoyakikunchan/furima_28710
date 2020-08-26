50c52b7bcd744b366a0c17304508b669d4b76a28
takoyakikunchan <66656447+takoyakikunchan@users.noreply.github.com>
2020-08-26 18:28:11 +0900

    フォームオブジェクトの修正

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -1,12 +1,12 @@
 class OrderAddress
 
   include ActiveModel::Model
-  attr_accessor  :postal_code, :prefecture_id, :city, :street, :building,:phone_number,:item_id,:user_id
+  attr_accessor   :token,:postal_code, :prefecture_id, :city, :street, :building,:phone_number,:item_id,:user_id
 
   def save
-    order = Order.create(user_id:user_id,item_id:user_id)
+    order = Order.create(user_id:user_id,item_id:item_id)
     # 住所の情報を保存
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, street: street, building: building,order_id: order.id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, street: street, building: building,order_id: order.id,phone_number: phone_number)
   end
 
 end
