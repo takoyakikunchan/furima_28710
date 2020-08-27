cce99a1f7fcfaca079455d9a5639423e04016e57
takoyakikunchan <66656447+takoyakikunchan@users.noreply.github.com>
2020-08-27 18:22:22 +0900

    factorybotの実装

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :order_address do
+    
+    postal_code { '123-4567' }
+    prefecture_id { 1 }
+    city { '東京都' }
+    street { '1-1' }
+    building { '東京ハイツ' }
+    phone_number{'09099999999'}
+    
+  end
+end
