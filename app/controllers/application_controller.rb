a2490a51b2931e798f268b7d51af81ecb0eeab05
takoyakikunchan <66656447+takoyakikunchan@users.noreply.github.com>
2020-08-28 11:22:43 +0900

    basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :basic_auth
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :given_name, :read_given_name, :read_fam_name, :birthday])
   end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
+  end
+  
 end
