195c3556a8c758aa445950dac97f17b862ec6c95
takoyakikunchan <66656447+takoyakikunchan@users.noreply.github.com>
2020-08-30 14:08:34 +0900

    自動デプロイの実装

---
+++ config/deploy.rb
@@ -0,0 +1,21 @@
+# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
+lock '3.14.1'
+
+# Capistranoのログの表示に利用する
+set :application, 'furima_28710'
+
+# どのリポジトリからアプリをpullするかを指定する
+set :repo_url,  'git@github.com:takoyakikunchan/furima_28710.git'
+
+# バージョンが変わっても共通で参照するディレクトリを指定
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、’2.6.5’ です
+
+# どの公開鍵を利用してデプロイするか
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/hamuchan.pem'] 
+
+# プロセス番号を記載したファイルの場所
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
