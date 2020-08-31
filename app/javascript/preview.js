df64448966944d7a2e3ca2bc933ee4e8974c884d
takoyakikunchan <66656447+takoyakikunchan@users.noreply.github.com>
2020-08-31 11:41:29 +0900

    プレビュー機能の実装

---
+++ app/javascript/preview.js
@@ -0,0 +1,19 @@
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+document.addEventListener('DOMContentLoaded', function(){
+  const imageList = document.getElementById('image-list');
+  document.getElementById('item-image').addEventListener('change', function(e){
+    const imageContent = document.querySelector('img');
+      if (imageContent){
+        imageContent.remove();
+      }
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+     const imageElement = document.createElement('div');
+     const blobImage = document.createElement('img');
+     blobImage.setAttribute('src', blob);
+     imageElement.appendChild(blobImage);
+    imageList.appendChild(imageElement);
+    blobImage.setAttribute('class', 'preview-image');
+  });
+});
+}
