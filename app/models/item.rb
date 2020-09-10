5746d6a11ee493ae8c17c08d2f50265149ffde9a
takoyakikunchan <66656447+takoyakikunchan@users.noreply.github.com>
2020-09-10 14:45:04 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :condition
   belongs_to_active_hash :shipping_fee_person
   belongs_to_active_hash :region
-  belongs_to_active_hash :date_ish
+  belongs_to_active_hash :date_ish                                                           
   has_many :item_tag_relations
   has_many :tags, through: :item_tag_relations, dependent: :destroy
   has_many :comments
