72bfa88f3baf33846e3d1aff5f30f70a94d8c720
takoyakikunchan <66656447+takoyakikunchan@users.noreply.github.com>
2020-09-24 17:33:35 +0900

    エラーの日本語表記の編集

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -25,39 +25,39 @@ describe Item do
       it 'カテゴリーの情報がないと保存できない' do
         @item.category_id = 0
         @item.valid?
-        expect(@item.errors.full_messages).to include("Categoryを入力してください")
+        expect(@item.errors.full_messages).to include("カテゴリーを入力してください")
       end
       it '商品の状態についての情報がないと保存できない' do
         @item.condition_id = 0
         @item.valid?
-        expect(@item.errors.full_messages).to include("Conditionを入力してください")
+        expect(@item.errors.full_messages).to include("商品の状態を入力してください")
       end
       it '配送料の負担についての情報がないと保存できない' do
         @item.shipping_fee_person_id = 0
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping fee personを入力してください")
+        expect(@item.errors.full_messages).to include("配送料の負担を入力してください")
       end
       it '発送元の地域についての情報がないと保存できない' do
         @item.region_id = 0
         @item.valid?
-        expect(@item.errors.full_messages).to include("Regionを入力してください")
+        expect(@item.errors.full_messages).to include("発送元の地域を入力してください")
       end
       it '発送までの日数についての情報がないと保存できない' do
         @item.date_ish_id = 0
         @item.valid?
-        expect(@item.errors.full_messages).to include("Date ishを入力してください")
+        expect(@item.errors.full_messages).to include("発送までの日数を入力してください")
       end
       it '価格についての情報がないと保存できない' do
         @item.price = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("値段を入力してください")
       end
-      it 'priceが300円未満では保存できないこと' do
+      it '価格が300円未満では保存できないこと' do
         @item.price = 200
         @item.valid?
         expect(@item.errors.full_messages).to include('値段は300~9,999,999円の間で設定してください')
       end
-      it 'priceが9,999,999円を超過すると保存できないこと' do
+      it '価格が9,999,999円を超過すると保存できないこと' do
         @item.price = 100_000_000
         @item.valid?
         expect(@item.errors.full_messages).to include('値段は300~9,999,999円の間で設定してください')
@@ -70,7 +70,7 @@ describe Item do
       it 'ジャンルの選択が「--」の時は商品は保存できない' do
         @item.date_ish_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('発送までの日数を選択してください')
+        expect(@item.errors.full_messages).to include('Date ishを選択してください')
       end
 
       it '商品名が40文字以上では保存できないこと' do
